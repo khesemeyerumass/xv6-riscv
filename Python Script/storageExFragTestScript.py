ffc4597aa2095888da47e4dbf0129f421f6d94f2
khesemeyerumass <60161543+khesemeyerumass@users.noreply.github.com>
2021-05-04 21:07:48 -0400

    Add files via upload

---
+++ Python Script/storageExFragTestScript.py
@@ -0,0 +1,21 @@
+# Kyle Hesemeyer
+
+import pexpect
+import os
+import subprocess
+
+# Script I used to run the external fragmentation test program that outputs data 
+# regarding the external fragmentation of the memory allocator that was compiled.
+# Note: This is only intended for the storage memory allocator
+xv6_process = pexpect.spawn("make qemu")
+fout = open('mylog.txt','wb')
+xv6_process.logfile = fout
+xv6_process.expect_exact("init: starting sh")
+xv6_process.expect_exact("$ ")
+
+# Test used to record Internal Fragmentation
+xv6_process.sendline('allocExFragTest')
+xv6_process.expect_exact("$ ")
+xv6_process.close()
+
+
